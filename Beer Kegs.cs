3abae57e39270f7a1b65a6bc3be5c855b01759a8
Simeon Minkov <115207716+Simeon999@users.noreply.github.com>
2023-06-15 19:11:44 +0300

    Add files via upload

---
+++ Beer Kegs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace pokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int kegsCount = int.Parse(Console.ReadLine());
+
+            string biggestKegModel = "";
+            double biggestKegVolume = 0;
+
+            for(int i = 0; i < kegsCount; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = int.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double volume = Math.PI * (radius*radius) * height;
+                if(biggestKegVolume < volume)
+                {
+                    biggestKegModel= model;
+                    biggestKegVolume = volume;
+                }
+            }
+            Console.WriteLine(biggestKegModel);
+        }
+    }
+}
